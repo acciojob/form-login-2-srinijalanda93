0b5cad658299d97c6331718389c71cf6b0aac8a7
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-21 21:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const formEleObj=document.querySelector("form");
+formEleObj.addEventListener("submit",(eventobj)=>{
+	 const data={
+		 FirstName:formEleObj.FirstName.value,
+		 LastName:formEleObj.LastName.value,
+		 PhoneNumber:formEleObj.PhoneNumber.value,
+		 EmailID:formEleObj.EmailID.value
+    };
+   // console.log(data);
+	alert(
+        "First Name: " + data.FirstName +
+        "\nLast Name: " + data.LastName +
+        "\nPhone Number: " + data.PhoneNumber +
+        "\nEmail ID: " + data.EmailID
+    );
+
+})
